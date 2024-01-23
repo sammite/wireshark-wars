adf7536ecf24805314034f8c5e1d59064b47086b
sammite <54330632+sammite@users.noreply.github.com>
2024-01-22 20:15:26 -0500

    Update packetsniffing.ps1

--- packetsniffing.ps1
+++ packetsniffing.ps1
@@ -1,4 +1,7 @@
 # Create SSLKEYLOG file and place in user accessible directory.
+# To threat hunt, you can search for boxes/users that have the SSLKEYLOGFILE set as an environment variable.
+# TBH, I don't think this is a particular concern for most people, most of the time. 
+
 
 # https://stackoverflow.com/questions/57498386/sslkeylogfile-environment-variable-doesnt-populate-any-text-file 
 # Reference
