e24c71038cacf61b08faca723873babe3c5097ef
sammite <54330632+sammite@users.noreply.github.com>
2024-01-22 19:42:31 -0500

    Create packetsniffing.ps1

---
+++ packetsniffing.ps1
@@ -0,0 +1,7 @@
+# Create SSLKEYLOG file and place in user accessible directory.
+
+# https://stackoverflow.com/questions/57498386/sslkeylogfile-environment-variable-doesnt-populate-any-text-file 
+# Reference
+
+# We'll use this to place our decrypted traffic keys
+SetX SSLKEYLOGFILE "C:\Users\Public\ssl.log"
